a9f862af31071689e3ebc9fe4e25f765e3bbf3f3
ambrosius calvin <calvambro@gmail.com>
2023-05-08 14:53:59 +0700

    openMusic V1.0)

---
+++ migrations/1683523840304_create-table-albums.js
@@ -0,0 +1,54 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('albums', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INT',
+      notNull: true,
+    },
+  });
+
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INT',
+      notNull: true,
+    },
+    genre: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    performer: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    duration: {
+      type: 'INT',
+    },
+    albumId: {
+      type: 'TEXT',
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('albums');
+  pgm.dropTable('songs');
+};
